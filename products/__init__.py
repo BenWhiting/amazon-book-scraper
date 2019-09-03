100984f607f7614c44643ff33b83768d58683857
Ben Whiting <benwhiting04@gmail.com>
2019-09-02 21:09:42 -0700

    more inital work

--- products/__init__.py
+++ products/__init__.py
@@ -1,4 +1,6 @@
 from .product import Product
+import csv
+from .search import *
 
 class Products(object):
     def __init__(self, product_dict_list=[]):
@@ -11,3 +13,22 @@ class Products(object):
     def _add_product(self, prodict_dict):
         p = Product(prodict_dict)
         self.products.append(p)
+
+    def __len__(self):
+        return len(self.products)
+
+    def __getitem(self, key):
+        return self.products[key]
+    
+    def csv(self, file_name, separator=","):
+        if not self.products:
+            return
+        
+        with open(file_name, 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=separator)
+
+            header = list(self.products[0].product.keys())
+            writer.writerow(header)
+
+            for product in self.products:
+                writer.writerow(list(product.product.values()))
---
+++ products/search.py
@@ -0,0 +1,2 @@
+def search (keywords="", search_url="", max_product=100):
+    return
