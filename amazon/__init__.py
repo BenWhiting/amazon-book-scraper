ad950d710fda6e7084b061848b2aeb43843ec4e6
Ben Whiting <benwhiting04@gmail.com>
2019-09-08 14:46:12 -0700

    removing unused code

--- amazon/__init__.py
+++ amazon/__init__.py
@@ -1,18 +1,6 @@
 import requests
 from amazon.client import Client
 
-_HOST_URL = "www.amazon.com"
-_DEFAULT_USER_AGENT = ""
-_CHROME_DEKSTOP_USER_AGENT = ""
-_ACCEPT = "text/html,application/xhtml+xml,\
-    application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
-
-
-_USER_AGENT_LIST = [
-    _DEFAULT_USER_AGENT,
-    _CHROME_DEKSTOP_USER_AGENT,
-]
-
 class Connection(object):
     def __init__(self, URL=""):
         self.url = URL
